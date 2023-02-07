45f021947632c9706a0458259b27af3fecba636b
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-02-07 13:29:28 +0300

    css - modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React from "react";
+import s from'./Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
         <img src='https://i.pinimg.com/originals/09/17/04/0917045fcee27f48e768680d1f800577.jpg' />
       </header>
     );
