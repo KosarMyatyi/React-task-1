14cd0bb61f8d60e609c0c9f56d42e476159587cb
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-02-07 12:20:33 +0300

    a

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://i.pinimg.com/originals/09/17/04/0917045fcee27f48e768680d1f800577.jpg' />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div>
+          <img src='https://klike.net/uploads/posts/2019-06/1561526578_1.jpg' />
+        </div>
+        <div>
+          ava + desc
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
