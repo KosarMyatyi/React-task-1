17932b3477cfeb2028589c78861a4625a49c9f3e
KosarMyatyi <nespovityi_oleg@mail.ru>
2023-02-24 17:14:59 +0300

    51-52. get user API;

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import styles from './Users.module.css'
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
+
+    let getUsers = () => 
+    {
+
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-                followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 2, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-                followed: true, fullName: 'Milana', status: 'I am a boss too', location: { city: 'Rim', country: 'Italy' }
-            },
-            {
-                id: 3, photoUrl: 'https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg',
-                followed: false, fullName: 'Sasha', status: 'I am a boss too', location: { city: 'Primopka', country: 'Russia' }
-            },
-        ]
-        )
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+
+            .then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
 
     return (
         <div>
+        <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map((u, index) => <div key={index}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto} />
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto} />
                         </div>
                         <div> {u.followed
                             ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
@@ -36,12 +35,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{ /* u.location.country */}</div>
+                            <div>{ /* u.location.city */}</div>
                         </span>
                     </span>
                 </div>)
